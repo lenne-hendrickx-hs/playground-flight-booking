0d2c0caddf3e66b02bce1d9af3cbc8ab2300b23c
Lenne Hendrickx <lenne.h@sparkcentral.com>
2024-07-26 12:07:57 +0200

    Style suggested replies title

--- frontend/components/SuggestedReplyList.tsx
+++ frontend/components/SuggestedReplyList.tsx
@@ -2,27 +2,31 @@ import React, {useEffect, useRef} from 'react';
 import SuggestedReply, {SuggestedReplyItem} from "./SuggestedReply";
 
 interface SuggestedReplyListProps {
-  suggestedReplies: SuggestedReplyItem[];
-  className?: string;
-  onReplySelected?: (text: string) => void
+    suggestedReplies: SuggestedReplyItem[];
+    className?: string;
+    onReplySelected?: (text: string) => void
 }
 
-export default function SuggestedReplyList({ suggestedReplies, className, onReplySelected }: SuggestedReplyListProps) {
-  const endOfMessagesRef = useRef<HTMLDivElement>(null);
+export default function SuggestedReplyList({suggestedReplies, className, onReplySelected}: SuggestedReplyListProps) {
+    const endOfMessagesRef = useRef<HTMLDivElement>(null);
 
-  // Automatically scroll down whenever the messages change
-  useEffect(() => {
-    if (endOfMessagesRef.current) {
-      endOfMessagesRef.current.scrollIntoView({ behavior: 'smooth' });
-    }
-  }, [suggestedReplies]);
+    // Automatically scroll down whenever the messages change
+    useEffect(() => {
+        if (endOfMessagesRef.current) {
+            endOfMessagesRef.current.scrollIntoView({behavior: 'smooth'});
+        }
+    }, [suggestedReplies]);
 
-  return (
-    <div className={`suggested-replies ${className}`}>
-      {suggestedReplies.map((reply, index) => (
-        <SuggestedReply key={index} suggestedReply={reply} className={'mb-m'} onReplySelected={onReplySelected}/>
-      ))}
-      <div ref={endOfMessagesRef} />
-    </div>
-  );
+    return (
+        <div className={`suggested-replies ${className}`}>
+            {suggestedReplies.length > 0 &&
+                <h3 className={'suggested-replies-title'}>Suggested replies</h3>
+            }
+            {suggestedReplies.map((reply, index) => (
+                <SuggestedReply key={index} suggestedReply={reply} className={'mb-m'}
+                                onReplySelected={onReplySelected}/>
+            ))}
+            <div ref={endOfMessagesRef}/>
+        </div>
+    );
 }
